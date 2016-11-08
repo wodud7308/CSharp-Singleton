10468e5e0fe77975472ee454f90733b0f4fe6566
wodud7308 <wodud7308@naver.com>
2016-11-08 20:08:07 +0900

    Singleton

---
+++ Singleton.cs
@@ -0,0 +1,17 @@
+public class Singleton<T> where T : new()
+    {
+
+        private static T Instance;
+
+        public static T GetInstance
+        {
+            get
+            {
+                if(Instance == null)
+                {
+                    Instance = new T();
+                }
+                return Instance;
+            }
+        }
+    }
